b973364dcb8e375cac9db4b9645af109ffbbafb0
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-05 17:47:19 +0200

    Update BackupServiceProvider.php

--- src/BackupServiceProvider.php
+++ src/BackupServiceProvider.php
@@ -8,18 +8,18 @@ use Laravel\Backup\Backup;
 
 class BackupServiceProvider extends ServiceProvider
 {
-  public function boot()
-  {
-    if ($this->app->runningInConsole()) {
-      $this->commands([Backup::class]);
-    }
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([Backup::class]);
+        }
 
-    $this->app->booted(function () {
-      $schedule = app(Schedule::class);
-      $schedule->command('p:backup')->->daily();
-    });
-  }
-  public function register()
-  {
-  }
+        $this->app->booted(function () {
+            $schedule = app(Schedule::class);
+            $schedule->command('p:backup')->daily();
+        });
+    }
+    public function register()
+    {
+    }
 }
